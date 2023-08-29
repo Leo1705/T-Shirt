9cd2fc992c991d9bff7170bde7ef93bf0e9e1351
Leo / <89755246+Leo1705@users.noreply.github.com>
2023-08-29 16:44:43 +0200

    Final Commit

---
+++ src/components/AIPicker.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AIPicker = () => {
+
+  return (
+    <>
+    <h1>
+    
+       Sorry, this currently doesn't work
+
+      </h1>
+      </>
+  )
+}
+
+export default AIPicker
